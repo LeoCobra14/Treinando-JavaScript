5041cff93283da49684941fb4a0ca3ba45ebe546
LeoCobra14 <lcobra6@outlook.com.br>
2025-03-12 13:34:31 -0300

    Exercicio JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função que imprime a mensagem no terminal
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executando a função três vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
